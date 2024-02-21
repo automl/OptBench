a296aacc903dc9a56de0975362f83d8e8e9de9b2
benjamc <c.benjamins@ai.uni-hannover.de>
2024-02-21 18:14:24 +0100

    refactor: rename configs

--- tests/test_optbench.py
+++ tests/test_optbench.py
@@ -16,3 +16,15 @@ def test_opt(funcclass):
     func = funcclass(dim=4)
     y = func._function(func.x_min)
     assert func.f_min == y
+
+
+
+from omegaconf import OmegaConf
+from hydra.utils import instantiate
+from smacbenchmarking.utils.trials import TrialInfo, TrialValue
+
+path = "../lib/OptBench/optbench/configs/problem/OptBench/Ackley_5.yaml"
+cfg = OmegaConf.load(path)
+cfg.problem.function.seed = 1
+problem = instantiate(cfg.problem)
+problem.evaluate(TrialInfo(config=problem.configspace().sample_configuration()))
