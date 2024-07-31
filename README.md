95d71fe473429d0a797d2d30ea28d74f6f08cfdc
benjamc <c.benjamins@ai.uni-hannover.de>
2024-07-31 21:01:10 +0200

    Update

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 
 Optimization tasks:
 Synthetic functions: Ackley, Levy, Schwefel
+
+Compatible with [carps](https://github.com/automl/CARP-S)
+
+## Installation
+```bash
+# Create conda env
+conda create -n optbench python=3.12 -c conda-forge
+
+# Activate env
+conda activate optbench
+
+# Clone repo
+git clone git@github.com:automl/OptBench.git
+cd OptBench
+
+# Install 
+pip install -e .
+```
+
+## Running an optimizer from carps
+The command for running an optimizer, e.g., random search from carps on OptBench looks as follows:
+```bash
+python -m carps.run 'hydra.searchpath=[pkg://optbench/configs]' +optimizer/randomsearch=config +problem/OptBench=synth_Ackley_2 seed=1
+```
+The breakdown of the command:
+- `'hydra.searchpath=[pkg://optbench/configs]'`: Let hydra know where to find the configs of the OptBench package. For this, `optbench` needs to be installed. It is in general: pkg://PACKAGE_NAME/PATH_INSIDE_PACKAGE_TO_CONFIGS_FOLDER .
+- `+optimizer/randomsearch=config`: select an optimizer from `carps`. Follows the config folder structure in `carps`. Beware, for other optimizers you need to install dependencies (check the [repo](https://github.com/automl/CARP-S)).
+- `+problem/OptBench=synth_Ackley_2`: Select a problem. Follows the configs folder structure in this package, starting from `optbench/configs`.
+- `seed=1`: Set the seed to 1. 🙂
+
+Of course, you can also specify the run dir etc.
+For more hydra overrides and parallelization, check their [docs/tutorials](https://hydra.cc/docs/advanced/override_grammar/basic/).
--- tests/test_optbench.py
+++ tests/test_optbench.py
@@ -5,9 +5,15 @@
 import inspect
 
 import pytest
+import numpy as np
 
 from optbench import functions
 from optbench.abstract_function import AbstractFunction
+from optbench.functions import Schwefel
+
+from omegaconf import OmegaConf
+from hydra.utils import instantiate
+from carps.utils.trials import TrialInfo
 
 function_classes = [c[1] for c in inspect.getmembers(functions, inspect.isclass) if issubclass(c[1], AbstractFunction) and c[1] != AbstractFunction]
 
@@ -15,16 +21,15 @@ function_classes = [c[1] for c in inspect.getmembers(functions, inspect.isclass)
 def test_opt(funcclass):
     func = funcclass(dim=4)
     y = func._function(func.x_min)
-    assert func.f_min == y
+    if funcclass == Schwefel:
+        # Schwefel optimum is weird although function is implemented according to source
+        assert np.isclose(func.f_min, y, atol=1e-4)
+    else:
+        assert np.isclose(func.f_min, y)
 
-
-
-from omegaconf import OmegaConf
-from hydra.utils import instantiate
-from carps.utils.trials import TrialInfo, TrialValue
-
-path = "../lib/OptBench/optbench/configs/problem/OptBench/Ackley_5.yaml"
-cfg = OmegaConf.load(path)
-cfg.problem.function.seed = 1
-problem = instantiate(cfg.problem)
-problem.evaluate(TrialInfo(config=problem.configspace().sample_configuration()))
+@pytest.mark.parametrize("path", ["optbench/configs/problem/OptBench/Ackley_5.yaml", "optbench/configs/problem/OptBench/Levy_5.yaml", "optbench/configs/problem/OptBench/Schwefel_5.yaml"])
+def test_instantiate_and_evaluate(path: str):
+    cfg = OmegaConf.load(path)
+    cfg.problem.function.seed = 1
+    problem = instantiate(cfg.problem)
+    problem.evaluate(TrialInfo(config=problem.configspace.sample_configuration()))
